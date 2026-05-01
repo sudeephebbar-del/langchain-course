d22adf2759870a1a39c99a879943df0a752de280
Sudeep Hebbar <sudeep.hebbar@gmail.com>
2026-04-30 23:18:45 -0400

    Initial commit for Search Agent

---
+++ main.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
